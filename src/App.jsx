a002d0a16a1735b3317995c5e8fc60ec866882c6
kaygamma <yahedmund00@gmail.com>
2026-08-18 00:56:24 +0100

    basic react structure

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,18 @@ function App() {
 
   return (
     <>
-      
+      <Header />
+      <div
+      className=' border flex flex-col gap-3 items-center justify-center h-80 w-100 hover:shadow-2xl rounded-3xl bg-linear-to-r from-cyan-500 to-blue-500 m-auto p-5 py-auto'
+      >
+        <button
+        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-4xl object-center' 
+        
+        >
+          Start
+        </button>{' '}
+        
+        </div>
     </>
   )
 }
